76c904c7a8f80d6282631db71726299c95ae4d09
Shannon Sullivan <Shannon@DeepThought.local>
2024-04-15 20:45:54 -0700

    Linking correctly

--- Chess/Piece.hpp
+++ Chess/Piece.hpp
@@ -1,7 +1,7 @@
 #ifndef Piece_h
 #define Piece_h
 
-#include "HelperFunctions.cpp"
+#include <vector>
 #include <iostream>
 
 using namespace std;
@@ -15,12 +15,27 @@ class Piece {
         char GetSymbol() { return symbol;};
         virtual bool ValidMove(int, int) = 0;
         virtual bool ValidCapture(int, int) = 0;
+        int CalculateStep(int position, int destination){
+            if (position < destination) return 1;
+            if (position > destination) return -1;
+            return 0;
+        }
+
+        vector<vector<int> > MovePath(int positionX, int positionY, int destinationX, int destinationY){
+            vector<vector<int> > path;
+            int stepX = CalculateStep(positionX, destinationX);
+            int stepY = CalculateStep(positionY, destinationY);
+
+            //for (int x = positionX + stepX, y = positionY + stepY; x != destinationX && y != destinationY; x += stepX, y += stepY)
+            //{
+            //}
+            return path;
+        }   
 
     private:
         string name = "";
         bool white;
         char symbol;
 };
-
 
 #endif
